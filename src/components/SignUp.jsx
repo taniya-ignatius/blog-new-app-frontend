e274e8006fbe4edd7d185dfe73a0234753cae1d8
taniya-ignatius <itaniya895@gmail.com>
2024-02-10 10:46:11 +0530

    Login success

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const SignUp = () => {
+  return (
+    <div><center>
+        <div className="container">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">EMAIL-ID</label>
+                            <input type="email" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">PASSWORD</label>
+                            <input type="password" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success">LOGIN</button>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <Link href='/'>New Users Click here</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div></center>
+    </div>
+  )
+}
+
+export default SignUp
